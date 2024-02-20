3ee1f071925ee75c7fac0db33577b3e38475ddd9
santicano <canosantiago404@gmail.com>
2024-02-20 14:19:06 -0300

    añadido api y toast para actualizar nombre de store

--- app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
@@ -3,7 +3,11 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 import * as z from "zod";
+import toast from "react-hot-toast";
+import axios from "axios";
+
 import { Store } from "@prisma/client";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -31,6 +35,9 @@ const formSchema = z.object({
 type SettingsFormValues = z.infer<typeof formSchema>;
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -40,7 +47,16 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   });
 
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log(data);
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success("Store updated");
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
